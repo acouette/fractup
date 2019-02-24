14f729acc388d6a0f03ab04e7a5a294a670e8eb0
Antoine Couette <a.j.couette@gmail.com>
2019-02-24 15:30:13 +0100

    update manual ec2

---
+++ manual_ec2/commands.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -u
+set -e
+
+INSTANCE_URL=$1
+
+ssh -o StrictHostKeyChecking=no -i "~/.aws/my-key-pair.pem" ubuntu@$INSTANCE_URL <<EOF
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs git
+git clone https://github.com/acouette/FractalJS.git
+cd FractalJS
+npm i
+npm run http-build
+ENABLE_CLUSTER=true node dist/js/node/http/server.js
+EOF
