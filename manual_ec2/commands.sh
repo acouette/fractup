dd2142988c2267dd1ee367854f682cb92d167ddd
Antoine Couette <antoine.couette.prestataire@it-ce.fr>
2019-02-24 21:53:03 +0100

    udpate command for mac + presentation

--- manual_ec2/commands.sh
+++ manual_ec2/commands.sh
@@ -4,7 +4,7 @@ set -e
 
 INSTANCE_URL=$1
 
-ssh -o StrictHostKeyChecking=no -i "~/.aws/my-key-pair.pem" ubuntu@$INSTANCE_URL <<EOF
+ssh -o StrictHostKeyChecking=no -i "~/.aws/my-itce-mac-keypair.pem" ubuntu@$INSTANCE_URL <<EOF
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs git
@@ -12,5 +12,5 @@ git clone https://github.com/acouette/FractalJS.git
 cd FractalJS
 npm i
 npm run http-build
-ENABLE_CLUSTER=true node dist/js/node/http/server.js
+node dist/js/node/http/server.js
 EOF
